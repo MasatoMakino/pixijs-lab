48646ae5f630391c1dcf894d3f72c4c8d8571dd7
MasatoMakino <unagiinu.g@gmail.com>
2021-02-16 15:43:07 +0900

    add : message.

--- demoSrc/onLoadImage.js
+++ demoSrc/onLoadImage.js
@@ -7,11 +7,16 @@ export function initApp(w = 800, h = 600) {
 }
 
 export function initLink() {
+  const message = document.createElement("div");
+  message.innerHTML = `This is a demo to check the behavior of img.texture.baseTexture.on("loaded") in pixi.js.<br>
+Use the link to Google above to navigate the page, and use your browser's back button to redisplay it.<br>
+If on("loaded") does not fire, the texture will be displayed in the upper left corner of the screen.`;
   const link = document.createElement("a");
   link.href = "https://google.com";
   link.innerHTML = "jump to google";
 
   const div = document.createElement("div");
   div.appendChild(link);
+  div.appendChild(message);
   document.body.appendChild(div);
 }
