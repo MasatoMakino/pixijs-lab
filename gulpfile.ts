c9e68d253dae5d6ca3ddb38c218e62e1e6743874
MasatoMakino <unagiinu.g@gmail.com>
2021-02-16 15:47:11 +0900

    clean up bundle script.

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,8 +1,7 @@
 const { parallel, series } = require("gulp");
 
-const doc = require("gulptask-tsdoc").get();
-const server = require("gulptask-dev-server").get("./docs/demo");
-const { bundleDemo, watchDemo } = require("gulptask-demo-page").get({
+const server = require("gulptask-dev-server").generateTask("./docs/demo");
+const { bundleDemo, watchDemo } = require("gulptask-demo-page").generateTasks({
   externalScripts: [],
 });
 
@@ -11,6 +10,5 @@ const watchTasks = async () => {
 };
 
 exports.start_dev = series(watchTasks, server);
-const bundle = parallel(bundleDemo, doc);
+const bundle = parallel(bundleDemo);
 exports.build = series(bundle);
-exports.build_clean = series(bundle);
